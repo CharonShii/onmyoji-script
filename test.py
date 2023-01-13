b4f6251a178da76668815db70eff88b07468a070
DESKTOP-SHI\CharonShi <charon.shi.dev@outlook.com>
2023-01-13 15:08:29 +0800

    1

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ print(a)
 b = pyautogui.center(a)
 print(b)
 
-# region(左、上、宽、高)，限定查找的区域
+# 参数region(左、上、宽、高)，限定查找的区域
 c = pyautogui.locateCenterOnScreen(ac.main_path + "/Snipaste_2023-01-13_14-06-44.png",
                                    region=(0, 0, round(size.width / 2), size.height), confidence=0.95)
 print(c)
