eb79bd7dffe2182c0b4f2bc6496242c76216242b
DESKTOP-SHI\CharonShi <charon.shi.dev@outlook.com>
2023-01-13 15:00:56 +0800

    1

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 import action as ac
 import loaction as la
+import pyautogui
 
-la.kun28_duiyuan()
+size = pyautogui.size()
+print(size)
+# la.kun28_duiyuan()
 
 # tansuo = ac.loading("/tiaozhan_yuhunduizhang.png")
 # print(tansuo)
+
+
+a = pyautogui.locateOnScreen(ac.main_path + "/Snipaste_2023-01-13_14-06-44.png",
+                             region=(0, 0, round(size.width / 2), size.height))
+print(a)
+b = pyautogui.center(a)
+print(b)
+
+# region(左、上、宽、高)，限定查找的区域
+c = pyautogui.locateCenterOnScreen(ac.main_path + "/Snipaste_2023-01-13_14-06-44.png",
+                                   region=(0, 0, round(size.width / 2), size.height), confidence=0.95)
+print(c)
